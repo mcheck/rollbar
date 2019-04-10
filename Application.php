b728f68462d9d41d400af1c2ba586bd5dda32291
Daniel <dfatkic@invisionpower.com>
2019-04-10 19:19:10 +0200

    Privacy Policy Support

--- Application.php
+++ Application.php
@@ -18,6 +18,19 @@ class _Application extends \IPS\Application
 {
 	PUBLIC CONST GITHUB_REPO = "https://github.com/DanielFatkic/rollbar";
 
+	/**
+	 * Get any third parties this app uses for the privacy policy
+	 *
+	 * @return array( title => language bit, description => language bit, privacyUrl => privacy policy URL )
+	 */
+	public function privacyPolicyThirdParties()
+	{
+		return [[
+			'title' => \IPS\Member::loggedIn()->language()->addToStack("rollbar"),
+			'description' => "We're using rollbar to monitor website errors",
+			'privacyUrl' => "https://docs.rollbar.com/docs/privacy-policy"
+		]];
+	}
 
 	protected function get__icon()
 	{
